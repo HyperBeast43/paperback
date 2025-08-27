4557739e5b5c300098a3070859075f1536f29e4a
TheSnaz <Snaz.Dev@protonmail.com>
2025-08-27 15:47:43 +0200

    (Deathseeker) now properly triggers only on enhanced cards

--- content/ego_gift/death_seeker.lua
+++ content/ego_gift/death_seeker.lua
@@ -9,7 +9,7 @@ PB_UTIL.EGO_Gift {
 
   ego_gift_calc = function(self, card, context)
     if context.individual and context.cardarea == G.play then
-      if SMODS.get_enhancements(context.other_card) and not SMODS.has_no_rank(context.other_card) then
+      if next(SMODS.get_enhancements(context.other_card)) and not SMODS.has_no_rank(context.other_card) then
         return {
           mult = context.other_card.base.nominal
         }
